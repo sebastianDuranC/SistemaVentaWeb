60305e90b11808e485727c4d02a5cdf1eeda4669
Sebastian Duran <sd8587793@gmail.com>
2025-04-17 11:46:43 -0400

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaVentaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
